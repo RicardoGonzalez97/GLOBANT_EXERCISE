6a36ec7d4ea49ce676c0cac1b7489752e0d91381
rick <0.ricardogonzalez.0@gmail.com>
2023-05-07 17:40:38 -0600

    Creacion de mensajes string

---
+++ messages/strings_msg.py
@@ -0,0 +1,8 @@
+class Messages:
+    data_saved ="Data saved successfully"
+    file_wrong= "File doesn't fit the requirements"
+    no_data="There is no data in the database for table "
+    no_backup="There is no backups for table "
+    name_wrong="The file name doesn't match the requirements"
+    backup_ok="Backup restored successfully"
+    duplicated_pk="Primary key cannot be duplicated"
